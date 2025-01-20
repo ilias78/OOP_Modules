409eb78092f661b0b03cd8a7ce89932dfade45c2
ilias78 <9022530@student.zadkine.nl>
2025-01-20 15:22:19 +0100

    opdracht21

---
+++ M3opdracht21/src/CheckingAccount.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace M3opdracht21;
+
+class CheckingAccount extends BankAccount
+{
+     private float $overdraftLimit;
+
+     public function setType(): void
+     {
+         $this->accountType = "checking";
+     }
+    
+
+     public function setOverdraftLimit(float $overdraftLimit): void
+     {
+         $this->overdraftLimit = $overdraftLimit;
+     }
+
+
+    public function withdraw(float $amount): string
+    {
+        if($this->validateWithdrawal($amount, $this->overdraftLimit)){
+           
+            return parent:: withdraw($amount);
+        } 
+        return "Je hebt onvoldoende geld om dit bedrag op te nemen";
+    }
+}
+
+
+
+    // public function __construct(string $accountNumber, string $accountHolder, float $balance, string $accountType, float $interestRate)
+    // {
+    //     $this->accountNumber = $accountNumber;
+    //     $this->accountHolder = $accountHolder;
+    //     $this->balance = $balance;
+    //     $this->accountType = $accountType;
+    //     $this->interestRate = $interestRate;
+    // }
+
+   
+
+    
+
---
+++ M3opdracht21/src/SavingsAccount.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace M3opdracht21;
+
+class SavingsAccount extends BankAccount
+{
+   
+ private float $interestRate;
+
+
+ public function __construct(string $accountNumber, string $accountHolder , float $balance, float $interestRate)
+ {
+    parent::__construct($accountNumber, $accountHolder, $balance);
+    $this->interestRate = $interestRate;
+ }
+ 
+ public function  setInterestRate( float $interestRate)
+ {
+     $this->interestRate = $interestRate;
+} 
+
+
+ public function setType(): void
+ {
+     $this->accountType = "savings";
+ }
+
+
+public function withdraw(float $amount): string
+{
+    if($this->validateWithdrawal($amount)){
+       
+        return parent:: withdraw($amount);
+    } 
+        return "Je hebt onvoldoende geld om dit bedrag op te nemen";
+    }
+}
+
+ 
+
+    // public function __construct(string $accountNumber, string $accountHolder, float $balance, string $accountType, float $interestRate)
+    // {
+    //     $this->accountNumber = $accountNumber;
+    //     $this->accountHolder = $accountHolder;
+    //     $this->balance = $balance;
+    //     $this->accountType = $accountType;
+    //     $this->interestRate = $interestRate;
+    // }
+
+    
+
+
